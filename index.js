3c910d3074ccf0835886005317c81f227ea0740d
Luciano Meneses <luciano-menezes2010@hotmail.com>
2020-04-06 13:25:53 -0300

    Middleware modificada

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ function CheckNumberReq(req, res, next) {
   next();
 }
 
+server.use(CheckNumberReq);
+
 //Verifica se o id do projeto existe
 
 function checkIdExist(req, res, next) {
@@ -35,7 +37,7 @@ function checkIdExist(req, res, next) {
 //dentro de um array no seguinte formato: { id: "1", title: 'Novo projeto', tasks: [] };
 //Certifique-se de enviar tanto o ID quanto o título do projeto no formato string com aspas duplas.
 
-server.post("/projects", CheckNumberReq, (req, res) => {
+server.post("/projects", (req, res) => {
   const projeto = req.body;
 
   projects.push(projeto);
@@ -45,13 +47,13 @@ server.post("/projects", CheckNumberReq, (req, res) => {
 
 //GET /projects: Rota que lista todos projetos e suas tarefas;
 
-server.get("/projects", CheckNumberReq, (req, res) => {
+server.get("/projects", (req, res) => {
   return res.json(projects);
 });
 
 //PUT /projects/:id: A rota deve alterar apenas o título do projeto com o id presente nos parâmetros da rota;
 
-server.put("/projects/:index", CheckNumberReq, checkIdExist, (req, res) => {
+server.put("/projects/:index", checkIdExist, (req, res) => {
   const { index } = req.params;
   const { title } = req.body;
 
@@ -62,7 +64,7 @@ server.put("/projects/:index", CheckNumberReq, checkIdExist, (req, res) => {
 
 //DELETE /projects/:id: A rota deve deletar o projeto com o id presente nos parâmetros da rota;
 
-server.delete("/projects/:index", CheckNumberReq, checkIdExist, (req, res) => {
+server.delete("/projects/:index", checkIdExist, (req, res) => {
   const { index } = req.params;
 
   projects.splice(index, 1);
@@ -73,21 +75,16 @@ server.delete("/projects/:index", CheckNumberReq, checkIdExist, (req, res) => {
 //POST /projects/:id/tasks: A rota deve receber um campo title e armazenar uma nova tarefa no array de
 //tarefas de um projeto específico escolhido através do id presente nos parâmetros da rota;
 
-server.post(
-  "/projects/:index/tasks",
-  CheckNumberReq,
-  checkIdExist,
-  (req, res) => {
-    const { title } = req.body;
-    const { task } = req.body;
-    const { index } = req.params;
+server.post("/projects/:index/tasks", checkIdExist, (req, res) => {
+  const { title } = req.body;
+  const { task } = req.body;
+  const { index } = req.params;
 
-    projects[index].title = title;
+  projects[index].title = title;
 
-    projects[index].task.push(task);
+  projects[index].task.push(task);
 
-    return res.json(projects[index]);
-  }
-);
+  return res.json(projects[index]);
+});
 
 server.listen(3001);
