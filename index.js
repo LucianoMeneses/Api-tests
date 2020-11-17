09bd4cf2b7333a5d8cafbeee0d5662f13f2b9188
Luciano Meneses <luciano-menezes2010@hotmail.com>
2020-11-17 06:58:44 -0300

    Refatorando

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 const express = require("express");
-
 const server = express();
-
 server.use(express.json());
 
-const projects = [];
-
-var Quantidade = 0;
-
-//Middlewares
-
-//Conta a quantidade de requisições foram feitas
-
-function CheckNumberReq(req, res, next) {
-  Quantidade = Quantidade + 1;
-
-  console.log("Quantidade de requisições realizadas: " + Quantidade);
-
-  next();
-}
-
-server.use(CheckNumberReq);
+const projects = [{
+  "id":"1",
+  "title":"titulo de testes",
+  "task":["task1", "task2", "task3"],
+},
+  {
+    "id":"2",
+    "title":"titulo de testes 2",
+    "task":["task1", "task2", "task3"],
+  },
+  {
+    "id":"3",
+    "title":"titulo de testes 3",
+    "task":["task1", "task2", "task3"],
+  }
+];
 
 //Verifica se o id do projeto existe
 
@@ -28,14 +25,16 @@ function checkIdExist(req, res, next) {
   const { index } = req.params;
 
   if (!projects[index]) {
-    return res.status(400).send("The Project does not exists");
+    return res.status(400).json({"message":"O projeto não existe"});
   }
   return next();
 }
 
-//POST /projects: A rota deve receber id e title dentro do corpo e cadastrar um novo projeto
-//dentro de um array no seguinte formato: { id: "1", title: 'Novo projeto', tasks: [] };
-//Certifique-se de enviar tanto o ID quanto o título do projeto no formato string com aspas duplas.
+server.get("/", (req, res) => {
+  return res.json({"message":"Bem vindo a api de testes"}).status(200);
+});
+
+//POST /projects
 
 server.post("/projects", (req, res) => {
   const { id, title } = req.body;
@@ -54,10 +53,17 @@ server.post("/projects", (req, res) => {
 //GET /projects: Rota que lista todos projetos e suas tarefas;
 
 server.get("/projects", (req, res) => {
-  return res.json(projects);
+
+  if(projects.length != 0){
+
+    return res.json(projects);
+
+  }
+  return res.json({"message":"Não há projetos"})
+  
 });
 
-//PUT /projects/:id: A rota deve alterar apenas o título do projeto com o id presente nos parâmetros da rota;
+//PUT /projects/:id
 
 server.put("/projects/:index", checkIdExist, (req, res) => {
   const { index } = req.params;
@@ -68,22 +74,26 @@ server.put("/projects/:index", checkIdExist, (req, res) => {
   return res.json(projects[index]);
 });
 
-//DELETE /projects/:id: A rota deve deletar o projeto com o id presente nos parâmetros da rota;
+//DELETE /projects/:index
 
-server.delete("/projects/:index", checkIdExist, (req, res) => {
+server.delete("/projects/:index", (req, res) => {
   const { index } = req.params;
 
-  projects.splice(index, 1);
+  if(projects.splice(index, 1).length == 0){
 
-  return res.json(projects);
+  return res.json({"message":"Projeto não encontrado"}).status(400);
+  } else
+
+  return res.send().status(200);
 });
 
-//POST /projects/:id/tasks: A rota deve receber um campo title e armazenar uma nova tarefa no array de
-//tarefas de um projeto específico escolhido através do id presente nos parâmetros da rota;
+//POST /projects/:id/tasks:
 
 server.post("/projects/:index/tasks", checkIdExist, (req, res) => {
   const { title, task } = req.body;
   const { index } = req.params;
+
+  console.log(task)
 
   projects[index].title = title;
 
